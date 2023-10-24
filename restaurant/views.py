33bb9361bf355fc618eccbcac34ba4001a88b327
Ben Herbert <benherbert1983@googlemail.com>
2023-10-24 19:39:48 +0200

    Setting up authentication

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,19 +3,24 @@ from rest_framework import generics
 from .models import Menu, BookingTable
 from .serializers import menuSerializer, bookingTableSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = menuSerializer  
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = menuSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = BookingTable.objects.all()
     serializer_class = bookingTableSerializer  
